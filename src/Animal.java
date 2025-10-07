0cf167cd3d2d8d72b37ff933dc151e17287cc4e9
fares8lefi <fareslefi4@gmail.com>
2025-10-07 19:27:07 +0200

    add getters and setters for animal class

--- src/Animal.java
+++ src/Animal.java
@@ -11,6 +11,41 @@ public class Animal {
         this.age = age;
         this.isMammal = isMammal;
     }
+    // getters
+    public String getName() {
+        return name;
+    }
+    public String getFamily() {
+        return family;
+    }
+    public int getAge() {
+        return  age ;
+    }
+    public boolean isMammal() {
+        return isMammal;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setFamily(String family) {
+        this.name = family;
+    }
+
+    public void setAge(int age) {
+        if(age <=0){
+            System.out.println("Invalid Age");
+        }
+        this.age = age;
+    }
+
+    public void setIsMammal(boolean isMammal) {
+        this.isMammal =  isMammal ;
+    }
+
+
     @Override
     public String toString() {
         return "Animal [Famille=" + family + ", Nom=" + name + ", Âge=" + age + ", Mammifère=" + isMammal + "]";
